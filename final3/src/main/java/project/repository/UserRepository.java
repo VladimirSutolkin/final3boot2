55f3008112c19f3eaf081d1057c39b86759de48c
VladimirSutolkin <sutolkinv@gmail.com>
2023-01-26 05:12:55 +0300

    Initial commit

---
+++ final3/src/main/java/project/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package project.repository;
+
+
+import project.model.User;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    List<User> getAllUsers();
+
+    void createUser(User user);
+
+    void updateUser(User user);
+
+    User readUser(long id);
+
+    User deleteUser(long id);
+}
---
+++ final3/src/main/java/project/service/UserService.java
@@ -0,0 +1,17 @@
+package project.service;
+
+
+import project.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+
+    User readUser(long id);
+
+    User deleteUser(long parseUnsignedInt);
+
+    void createOrUpdateUser(User user);
+}
